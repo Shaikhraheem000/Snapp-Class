5e721b6bf312f70da4a2afc58366352b4d850810
Shaikhraheem000 <shaikhraheemr03@gmail.com>
2026-05-23 18:13:11 +0530

    Final Commit

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -4,7 +4,7 @@ import io
 
 @st.dialog('Share "Subject Enrollment" Link')
 def share_subject_dialog(subject_name, subject_code):
-    app_domain = "http://localhost:8501"
+    app_domain = "snapclass.streamlit.app"
     enroll_link = f"{app_domain}?join-code={subject_code}"
 
     st.header("Scan to Join")
